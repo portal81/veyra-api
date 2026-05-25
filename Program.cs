495213a3aa9312a21720693a0b01b31e89c4dd33
Mohamed <zagazig5872@gmail.com>
2026-05-25 18:50:20 +0300

    Use pooler port 6543, add error handling

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ using VeyraApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Database
+// Database - use pooler port 6543 for IPv4/IPv6 compatibility
 var supabaseUrl = builder.Configuration["NEXT_PUBLIC_SUPABASE_URL"] ?? "";
 var dbPassword = builder.Configuration["SUPABASE_DB_PASSWORD"] ?? "";
 var refName = supabaseUrl.Replace("https://", "").Replace(".supabase.co", "");
-var connectionString = $"Host=db.{refName}.supabase.co;Port=5432;Database=postgres;Username=postgres;Password={dbPassword};SSL Mode=Require;Trust Server Certificate=true";
+var connectionString = $"Host=db.{refName}.supabase.co;Port=6543;Database=postgres;Username=postgres;Password={dbPassword};SSL Mode=Require;Trust Server Certificate=true;Pooling=true;Timeout=15;";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
@@ -46,7 +46,12 @@ builder.Services.AddScoped<ICalculatorService, CalculatorService>();
 builder.Services.AddScoped<IHazemService, GroqService>();
 builder.Services.AddHttpClient<IHazemService, GroqService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -54,6 +59,20 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    try
+    {
+        await next(context);
+    }
+    catch (Exception ex)
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsJsonAsync(new { error = ex.Message, type = ex.GetType().Name });
+    }
+});
 
 app.UseCors();
 app.UseAuthentication();
